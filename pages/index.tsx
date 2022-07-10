b73fdfd7c6de058871939bfaf35f4d42a905ab67
onlinesuksesabadi <feyberho@gmail.com>
2022-07-11 04:11:35 +0700

    setting typescript

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from '../node_modules/next/head'
+import Image from '../node_modules/next/image'
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl'>NEXT JS</h1>
+      </div>
+    </>
+  )
+}
